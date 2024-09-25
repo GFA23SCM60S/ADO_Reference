ddb6b0f605aedb8b27e4e80db4348b13ca47ef11
Vinay <gvinay90@gmail.com>
2024-09-25 10:33:37 -0700

    modify the file

--- Group15_Submission/Ass1/test_assign1_1.c
+++ Group15_Submission/Ass1/test_assign1_1.c
@@ -17,13 +17,6 @@ char *testName;
 static void testCreateOpenClose(void);
 static void testSinglePageContent(void);
 
-static void testofgetblockPos(void);
-static void testBlockMapReading(void);
-static void testofWriteCurrentBlock(void);
-static void testofAppendEmptyBlock(void);
-static void testofEnsureCapacity(void);
-
-
 /* main function running all tests */
 int
 main (void)
@@ -35,13 +28,6 @@ main (void)
   testCreateOpenClose();
   testSinglePageContent();
 
-  printf("\033[1;3;34m Additional Tests \033[0m\n");
-  testofgetblockPos();
-  testBlockMapReading();
-  testofWriteCurrentBlock();
-  testofAppendEmptyBlock();
-  testofEnsureCapacity();
-  printf("\033[32mALL TESTS ARE PASSED SUCCESSFULLY !\n\033[0m");
   return 0;
 }
 
@@ -106,141 +92,9 @@ testSinglePageContent(void)
   for (i=0; i < PAGE_SIZE; i++)
     ASSERT_TRUE((ph[i] == (i % 10) + '0'), "character in page read from disk is the one we expected.");
   printf("reading first block\n");
-  TEST_CHECK(closePageFile (&fh));
 
   // destroy new page file
   TEST_CHECK(destroyPageFile (TESTPF));  
   
   TEST_DONE();
-}
-
-void testofgetblockPos(void) {
-    SM_FileHandle fh;
-
-    TEST_CHECK(createPageFile(TESTPF));
-    TEST_CHECK(openPageFile(TESTPF, &fh));
-    
-    // After opening, the current block position should be 0
-    ASSERT_TRUE(getBlockPos(&fh) == 0, "After opening a file, the current block position is not zero.");
-
-    TEST_CHECK(closePageFile(&fh));
-    TEST_CHECK(destroyPageFile(TESTPF));
-
-    TEST_DONE();
-}
-
-
-/* Test reading previous, current, next, and last block */
-void testBlockMapReading(void) {
-    SM_FileHandle fh;
-    SM_PageHandle ph = (SM_PageHandle)malloc(PAGE_SIZE);
-
-    // Setup - Create a file and write some data to multiple pages
-    TEST_CHECK(createPageFile(TESTPF));
-    TEST_CHECK(openPageFile(TESTPF, &fh));
-    for(int i = 0; i < 4; i++) { // ensure 4 pages exist
-        TEST_CHECK(appendEmptyBlock(&fh));
-
-    }
-
-    TEST_CHECK(writeBlock(4, &fh, ph)); // write to the last block to increase total pages
-
-    // Test reading the last block
-    TEST_CHECK(readLastBlock(&fh, ph));
-    ASSERT_TRUE(getBlockPos(&fh) == 4, "It did not move to the last block");
-
-    // Test reading the next block from position 4 should fail as it does not exist
-    ASSERT_TRUE(readNextBlock(&fh, ph) != RC_OK, "Non-existing next block");
-
-    // Test reading the previous block from position 4, should succeed
-    TEST_CHECK(readPreviousBlock(&fh, ph));
-    ASSERT_TRUE(getBlockPos(&fh) == 3, "It did not move to the previous block");
-
-    // Test reading the current block
-    TEST_CHECK(readCurrentBlock(&fh, ph));
-    ASSERT_TRUE(getBlockPos(&fh) == 3, "Current block position is incorrect");
-
-    // Move to first block and test reading the next block
-    TEST_CHECK(readFirstBlock(&fh, ph));
-    TEST_CHECK(readNextBlock(&fh, ph));
-    ASSERT_TRUE(getBlockPos(&fh) == 1, "It did not move to the next block after the first block");
-
-    TEST_CHECK(closePageFile(&fh));
-    TEST_CHECK(destroyPageFile(TESTPF));
-
-    TEST_DONE();
-}
-
-/* Test writing to the current block */
-void testofWriteCurrentBlock(void) {
-    SM_FileHandle fh;
-    SM_PageHandle ph = (SM_PageHandle)malloc(PAGE_SIZE);
-
-    // Initialize page handle with some data
-    for (int i = 0; i < PAGE_SIZE; i++) {
-      ph[i] = 'A';
-    }
-
-    TEST_CHECK(createPageFile(TESTPF));
-    TEST_CHECK(openPageFile(TESTPF, &fh));
-
-    // Write to the current block (which is the first block)
-    TEST_CHECK(writeCurrentBlock(&fh, ph));
-    // Read back to verify
-    TEST_CHECK(readFirstBlock(&fh, ph));
-    for (int i = 0; i < PAGE_SIZE; i++) {
-        ASSERT_TRUE(ph[i] == 'A', "Data written and read from the current block are not matching");
-
-    }
-
-    TEST_CHECK(closePageFile(&fh));
-    TEST_CHECK(destroyPageFile(TESTPF));
-    free(ph);
-
-    TEST_DONE();
-}
-
-/* Test appending an empty block */
-void testofAppendEmptyBlock(void) {
-    SM_FileHandle fh;
-    int initialPages;
-
-    TEST_CHECK(createPageFile(TESTPF));
-    TEST_CHECK(openPageFile(TESTPF, &fh));
-
-    initialPages = fh.totalNumPages;
-    
-    // Append an empty block and verify the total number of pages increased
-    TEST_CHECK(appendEmptyBlock(&fh));
-    ASSERT_TRUE(fh.totalNumPages == initialPages + 1, "Total number of pages did not increase after appending an empty block");
-
-    TEST_CHECK(closePageFile(&fh));
-    TEST_CHECK(destroyPageFile(TESTPF));
-
-    TEST_DONE();
-}
-
-/* Test ensuring capacity */
-void testofEnsureCapacity(void) {
-    SM_FileHandle fh;
-    int requiredPages = 5;
-
-    TEST_CHECK(createPageFile(TESTPF));
-    TEST_CHECK(openPageFile(TESTPF, &fh));
-
-    // Ensure the file has at least 5 pages
-    TEST_CHECK(ensureCapacity(requiredPages, &fh));
-    
-    // Verify the file now has at least 5 pages
-    ASSERT_TRUE(fh.totalNumPages >= requiredPages, "Even after verifying capacity, the file does not have the necessary number of pages.");
-
-    // Try reading the last page to ensure it exists
-    SM_PageHandle ph = (SM_PageHandle)malloc(PAGE_SIZE);
-    TEST_CHECK(readBlock(requiredPages - 1, &fh, ph));
-    free(ph);
-
-    TEST_CHECK(closePageFile(&fh));
-    TEST_CHECK(destroyPageFile(TESTPF));
-
-    TEST_DONE();
 }
