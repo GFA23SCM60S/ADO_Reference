1065b4c12a97b3f89f46ef4e03727a7fab2a26fc
shraddhaa9-hawk <srakholiya.hawk.iit.edu>
2024-10-13 04:01:45 -0500

    minor modification record mgr

--- Group15_Submission/assign3_record_manager/record_mgr.c
+++ Group15_Submission/assign3_record_manager/record_mgr.c
@@ -5,6 +5,12 @@
 #include "buffer_mgr.h"
 #include "storage_mgr.h"
 
+// mac number of pages
+const int maxNumberOfPages = 100;
+
+// Size of attribute's name
+const int attributeSize = 15;
+
 // Custom data structure to define the Record Manager.
 typedef struct RecordManager
 {
@@ -16,7 +22,7 @@ typedef struct RecordManager
 	int scanCount;
 	// PageHandle for using Buffer Manager to access the page files
 	BM_PageHandle pageHandle;
-	// Buffer Pool	
+	// Buffer Pool
 	BM_BufferPool bufferPool;
 	// Record ID	
 	RID recordID;
@@ -24,13 +30,9 @@ typedef struct RecordManager
 	Expr *condition;
 } RecordManager;
 
-const int maxNumberOfPages = 100;
-
-// Size of attribute's name
-const int attributeSize = 15; 
-
 RecordManager *recordManager;
 
+/* helper functions */
 int getFreeSpace(char* data, int sizeOfRecord) {
 	for (int i = 0; i < PAGE_SIZE / sizeOfRecord; i++) {
 		if (!(data[i * sizeOfRecord] == '+')) {
@@ -40,52 +42,40 @@ int getFreeSpace(char* data, int sizeOfRecord) {
 	return -1;
 }
 
-
-/*
-	- description : Initialization of the Record Manager
-	- return : RC code
-*/
 extern RC initRecordManager (void *mgmtData)
 {
 	// Initiliazing Storage Manager
 	initStorageManager();
+	printf("[init]: Record manager initialized success!\n");
 	return RC_OK;
 }
 
-/*
-	- description : Shuts down the record manager 
-	- return : RC code
-*/
 extern RC shutdownRecordManager ()
 {
 	recordManager = NULL;
 	free(recordManager);
+	printf("[shutdown]: Record manager shutdown success!\n");
 	return RC_OK;
 }
 
-/*
-	- description : Create a TABLE with table name "name" having schema specified by "schema"
-	- param : 
-		1. name - name of the table
-		2. schema - schema of the table 
-	- return : RC code
-*/
 extern RC createTable (char *name, Schema *schema)
 {
+	char data[PAGE_SIZE];
+	RC result;
+	char *pageHandle = data;
+	SM_FileHandle fileHandle;
+
 	// Allocating memory space to the record manager custom data structure
 	recordManager = (RecordManager*) malloc(sizeof(RecordManager));
+
+    // initialized record manager memory to zero
+    memset(recordManager, 0, sizeof(RecordManager));
 
 	// Initalizing the Buffer Pool using LFU page replacement policy
 	initBufferPool(&recordManager->bufferPool, name, maxNumberOfPages, RS_LRU, NULL);
 
-	char data[PAGE_SIZE];
-	int output;
-	char *pageHandle = data;
-	 
-	// Setting no. of tuples to 0
-	*(int*)pageHandle = 0; 
-
 	// Setting pageHandle intial value
+	*(int*)pageHandle = 0;
 	pageHandle = pageHandle + sizeof(int);
 	*(int*)pageHandle = 1;
 	pageHandle = pageHandle + sizeof(int);
@@ -111,42 +101,40 @@ extern RC createTable (char *name, Schema *schema)
 		pageHandle = pageHandle + sizeof(int);
     }
 
-	SM_FileHandle fileHandle;
-		
 	// Creating a page file as table name using the storage manager
-	if((output = createPageFile(name)) != RC_OK)
-		return output;
-		
+	if((result = createPageFile(name)) != RC_OK) {
+	    printf("[createTable]: create page file failed!\n");
+	    return result;
+	}
+
 	// Opening the newly created page using the storage manager
-	if((output = openPageFile(name, &fileHandle)) != RC_OK)
-		return output;
-	
+	if((result = openPageFile(name, &fileHandle)) != RC_OK) {
+		printf("[createTable]: open page file failed!\n");
+		return result;
+	}
+
 	// Write to first location of the page file using the storage manager
-	if((output = writeBlock(0, &fileHandle, data)) != RC_OK)
-		return output;
-		
+	if((result = writeBlock(0, &fileHandle, data)) != RC_OK) {
+		printf("[createTable]: write block failed!\n");
+		return result;
+	}
+
 	// Closing the file after writing to file using the storage manager
-	if((output = closePageFile(&fileHandle)) != RC_OK)
-		return output;
+	if((result = closePageFile(&fileHandle)) != RC_OK) {
+		printf("[createTable]: close page file failed!\n");
+		return result;
+	}
 
 	return RC_OK;
 }
 
-// This function opens the table with table name "name"
-/*
-	- description : Opens the table with table name "name"
-	- param : 
-		1. rel - pointer to the table into which will insert
-		2. name - name of the table
-	- return : RC code
-*/
 extern RC openTable (RM_TableData *rel, char *name)
 {
-	SM_PageHandle pageHandle;    
-	
 	int attrCount;
-	
-	// Setting the meta data of the table and name using the record manager meta data structure
+	SM_PageHandle pageHandle;
+	Schema *schema;
+
+	// Setting the meta data of the table and name
 	rel->mgmtData = recordManager;
 	rel->name = name;
     
@@ -155,7 +143,7 @@ extern RC openTable (RM_TableData *rel, char *name)
 	
 	// Setting the pointer intially to the 0th location
 	pageHandle = (char*) recordManager->pageHandle.data;
-	
+
 	// Retrieving total no. of tuples from the page file
 	recordManager->tuplesCount= *(int*)pageHandle;
 	pageHandle = pageHandle + sizeof(int);
@@ -167,8 +155,6 @@ extern RC openTable (RM_TableData *rel, char *name)
 	// Retriving the number of attributes from the page file
     attrCount = *(int*)pageHandle;
 	pageHandle = pageHandle + sizeof(int);
- 	
-	Schema *schema;
 
 	// Allocating memory space in schema
 	schema = (Schema*) malloc(sizeof(Schema));
@@ -209,12 +195,6 @@ extern RC openTable (RM_TableData *rel, char *name)
 	return RC_OK;
 }   
 
-/*
-	- description : Close the table
-	- param : 
-		1. rel - pointer to the table from which to delete
-	- return : RC code
-*/
 extern RC closeTable (RM_TableData *rel)
 {
 	// Storing the table's meta data
@@ -224,12 +204,6 @@ extern RC closeTable (RM_TableData *rel)
 	return RC_OK;
 }
 
-/*
-	- description : Deletes the table
-	- param : 
-		1. name - name of the table
-	- return : RC code
-*/
 extern RC deleteTable (char *name)
 {
 	// Removing the page file from memory
@@ -238,13 +212,6 @@ extern RC deleteTable (char *name)
 	return RC_OK;
 }
 
-
-/*
-	- description : Returns the no. of records(tuples) in the table
-	- param : 
-		1. rel - pointer to the table from which to delete
-	- return : RC code
-*/
 extern int getNumTuples (RM_TableData *rel)
 {
 	// Accessing our data structure's tuplesCount and returning it
@@ -252,13 +219,6 @@ extern int getNumTuples (RM_TableData *rel)
 	return recordManager->tuplesCount;
 }
 
-/*
-	- description : Create a new record in the table rel and sets the id in the record. 
-	- param : 
-		1. rel - pointer to the table into which will insert
-		2. record - the original record created with the data to insert
-	- return : RC code
-*/
 extern RC insertRecord (RM_TableData* rel, Record* record) {
 	// Set the reference ID for the new record
 	RecordManager *recordMngr = rel->mgmtData;	
@@ -304,13 +264,6 @@ extern RC insertRecord (RM_TableData* rel, Record* record) {
 	return RC_OK;
 }
 
-/*
-	- description : Delete the record with id from the table rel. 
-	- param : 
-		1. rel - pointer to the table from which to delete
-		2. id - the id of the record to delete
-	- return : RC code
-*/
 extern RC deleteRecord (RM_TableData* rel, RID id) {
 	// Pin page to prevent flushing and mark dirty before we are modifying it
 	RecordManager *recordMngr = rel->mgmtData;
@@ -331,14 +284,6 @@ extern RC deleteRecord (RM_TableData* rel, RID id) {
 	return RC_OK;
 }
 
-
-/*
-	- description : Update the record in the table rel 
-	- param : 
-		1. rel - pointer to the table into which will update the data
-		2. record - the original record to update with the data to insert
-	- return : RC code
-*/
 extern RC updateRecord (RM_TableData* rel, Record* record) {	
 	// Pin page to prevent flushing and mark dirty before we are modifying it
 	RecordManager *recordMngr = rel->mgmtData;
@@ -361,15 +306,6 @@ extern RC updateRecord (RM_TableData* rel, Record* record) {
 	return RC_OK;	
 }
 
-
-/*
-	- description : Get the record from the table rel 
-	- param : 
-		1. rel - pointer to the table with the record data
-		2. id - the id of the record to retrieve
-		3. record - the record that will hold the data
-	- return : RC code
-*/
 extern RC getRecord (RM_TableData* rel, RID id, Record* record) {
 	// Pin page to prevent flushing
 	RecordManager* recordMngr = rel->mgmtData;
@@ -391,15 +327,6 @@ extern RC getRecord (RM_TableData* rel, RID id, Record* record) {
 	return RC_OK;
 }
 
-// Scan all the records using the condition check
-/*
-	- description : Get the record from the table rel 
-	- param : 
-		1. rel - pointer to the table with the record data
-		2. scan - object to RM_ScanHandleobject to RM_ScanHandle
-		3. cond - condition for scanning the records in the table
-	- return : RC code
-*/
 extern RC startScan (RM_TableData *rel, RM_ScanHandle *scan, Expr *cond)
 {
 	// cond condition checking
@@ -442,13 +369,6 @@ extern RC startScan (RM_TableData *rel, RM_ScanHandle *scan, Expr *cond)
 		
 }
 
-/*
-	- description : Scans each record in the table and stores the resulted record in the record location
-	- param : 
-		1. scan - object to RM_ScanHandle
-		2. record - the original record created with the data to insert
-	- return : RC code
-*/
 extern RC next (RM_ScanHandle *scan, Record *record)
 {
 	// Initiliazing scan data
@@ -542,13 +462,6 @@ extern RC next (RM_ScanHandle *scan, Record *record)
 	return RC_RM_NO_MORE_TUPLES;
 }
 
-
-/*
-	- description : Close the scanning process
-	- param : 
-		1. scan - object to RM_ScanHandle
-	- return : RC code
-*/
 extern RC closeScan (RM_ScanHandle *scan)
 {
 	RecordManager *scanManager = (*scan).mgmtData;
@@ -573,12 +486,6 @@ extern RC closeScan (RM_ScanHandle *scan)
 	return RC_OK;
 }
 
-/*
-	- description : Returns the size of records in the schema
-	- param : 
-		1. schema - schema of the table schema of the table
-	- return : RC code
-*/
 extern int getRecordSize (Schema *schema)
 {
 	int length = 0, i; // Initial Value set to 0
@@ -601,17 +508,6 @@ extern int getRecordSize (Schema *schema)
 	return length+1;
 }
 
-/*
-	- description : Creates new schema
-	- param : 
-		1. numAttr - no. of attributes
-		2. attrNames - name of attributes
-		3. dataTypes - data types of schema
-		4. typeLength - length of the attribute
-		5. keySize - size of the key
-		6. keys - no. of keys
-	- return : RC code
-*/
 extern Schema *createSchema (int numAttr, char **attrNames, DataType *dataTypes, int *typeLength, int keySize, int *keys)
 {
 	// Allocate memory space to schema
@@ -627,25 +523,12 @@ extern Schema *createSchema (int numAttr, char **attrNames, DataType *dataTypes,
 	return tempSchema;
 }
 
-/*
-	- description : Free up the schema space
-	- param : 
-		1. schema - schema of the table schema of the table
-	- return : RC code
-*/
 extern RC freeSchema (Schema *schema)
 {
 	free(schema);
 	return RC_OK;
 }
 
-/*
-	- description : Creates a new record in the schema referenced by "schema"
-	- param : 
-		1. record - the original record created with the data to insert
-		2. schema - schema of the table schema of the table
-	- return : RC code
-*/
 extern RC createRecord (Record **record, Schema *schema)
 {
 	// Allocate memory for the new record
@@ -669,15 +552,6 @@ extern RC createRecord (Record **record, Schema *schema)
 	return RC_OK;
 }
 
-/*
-	- description : Sets the offset from initial position to specified attribute of the record 
-	into the 'result' parameter
-	- param : 
-		1. schema - schema of the table schema of the table
-		2.  attrNum - number of attributesnumber of attributes
-		3. result - stores the result 
-	- return : RC code
-*/
 RC attrOffset (Schema *schema, int attrNum, int *result)
 {
 	int i;
@@ -701,12 +575,7 @@ RC attrOffset (Schema *schema, int attrNum, int *result)
 	}
 	return RC_OK;
 }
-/*
-	- description : Removes the record from the memory.
-	- param : 
-		1. record - the original record created with the data to insert
-	- return : RC code
-*/
+
 extern RC freeRecord (Record *record)
 {
 	// De-allocating memory space allocated to record and freeing up that space
@@ -714,15 +583,6 @@ extern RC freeRecord (Record *record)
 	return RC_OK;
 }
 
-/*
-	- description : Retrieves an attribute from the given record in the specified schema
-	- param : 
-		1. record - the original record created with the data to insert
-		2. schema - schema of the table schema of the table
-		3.  attrNum - number of attributesnumber of attributes
-		4. value - value of the data types value of the data types
-	- return : RC code
-*/
 extern RC getAttr (Record *record, Schema *schema, int attrNum, Value **value)
 {
 	int varOffset = 0;
@@ -791,16 +651,6 @@ extern RC getAttr (Record *record, Schema *schema, int attrNum, Value **value)
 	return RC_OK;
 }
 
-
-/*
-	- description : Sets the attribute value in the record in the specified schema
-	- param : 
-		1. record - the original record created with the data to insert
-		2. schema - schema of the table 
-		3.  attrNum - number of attributes
-		4. value - value of the data types 
-	- return : RC code
-*/
 extern RC setAttr (Record *record, Schema *schema, int attrNum, Value *value)
 {
 	int varOffset = 0;
